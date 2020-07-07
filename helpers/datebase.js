4ed7af2e64d11ea219ecaa2a7f6ce604768cccf1
lsquintania <lsquintania@quintania-mac.local>
2020-07-07 12:28:27 -0300

    Adicionado arquivo heroku

--- helpers/datebase.js
+++ helpers/datebase.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(
-      'mongodb+srv://userAtlas:123@cluster0-wumzq.mongodb.net/grades?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@cluster0-wumzq.mongodb.net/grades?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import { studentRouter } from './routes/studentRouter.js';
 import { connectDB } from './helpers/datebase.js';
 const app = express();
 
+//require('dotenv').config();
+
 app.use(express.json());
 app.use(studentRouter);
 
-app.listen(3000 || 5000, '0.0.0.0', () => {
+app.listen(process.env.PORT, () => {
   global.connection = connectDB();
   console.log('API Iniciada');
 });
